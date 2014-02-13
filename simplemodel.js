076d4f03d8e4875c03917232d2a06a5a9668f9ba
Sergio Alberto Vázquez <alb.vazq@gmail.com>
2014-02-13 17:10:13 -0600

    Changed the way to create a new extension of the model
    Now the model is a class like, not an object type.

--- simplemodel.js
+++ simplemodel.js
@@ -1,127 +1,162 @@
-var SimpleModel = {
-    'extend': function(object) {
-        var model = {
-            'instance': {
-                'id': undefined,
-                'url': '',
-                'autopublish': true,
-                'isnew': false,
-                'methods': {
-                    'create': '',
-                    'save': '',
-                    'delete': ''
-                },
-                'data': {},
-                'required': [],
-                'getField': function(name) {
-                   SimpleLog.log('.'+this.id+'-'+name);
-                    var f = jQuery('.'+this.id+'-'+name);
-                    return f;
-                },
-                'publish': function() {
-                    var required = this.required;
-                    for(r in required) {
-                        if(this.data[required[r]] === '' || this.data[required[r]] === undefined) {
-                            var f = this.getField(required[r]).addClass('required-error');
-                            ret = false;
-                        } else {
-                            this.getField(required[r]).removeClass('required-error');
-                        }
+var SimpleModel = function(p) {
+    var properties = {
+        'url': undefined,
+        'actions': {
+            'insert': undefined,
+            'update': undefined,
+            'delete': undefined
+        },
+        'methods': {
+            'before': undefined,
+            'done': undefined,
+            'error': undefined
+        },
+        'fields': undefined
+    };
+    this.validator = function(i) {
+        if( jQuery(i).hasClass("border-error") )
+            jQuery(i).removeClass("border-error");
+        if( jQuery(i).hasClass("border-warning") )
+            jQuery(i).removeClass("border-warning");
+        if( properties.fields[jQuery(i).attr("name")].required !== undefined ) {
+            /*Validar contenido*/
+            if( i.value === '' || i.value === undefined) {
+                jQuery(i).addClass("border-error");
+                return 1;
+            }
+        }
+        if( properties.fields[jQuery(i).attr("name")].type !== undefined ) {
+            /*Validar tipo*/
+            if( typeOf(i.value) !== properties.fields[jQuery(i).attr("name")].type ) {
+                jQUery(i).addClass("border-warning");
+                return 1;
+            }
+        }
+        return 0;
+    };
+    this.instances = {};
+    this.create = function(data, autopublish) {
+        var fields = properties.fields;
+        var methods = properties.methods;
+        var actions = properties.actions;
+        var url = properties.url;
+        var d = new Date();
+        var rand = 0;
+        var parent = this;
+        while(rand === 0) {
+            rand = parseInt(Math.random()*10);
+        }
+        d = d.getTime()/100000;
+        d = ((d - parseInt(d))*100000)*rand;
+        /*Instance, this will be returned to manage the new object that belongs to the model*/
+        var oData = {
+            'id': parseInt(d),
+            'fields': {},
+            'isnew': false,
+            'before': undefined,
+            'inputs': [],
+            'publish': function(validate) {
+                var e = 0;
+                if(validate) {
+                    for(i in this.inputs) {
+                        e = e+parent.validator(this.inputs[i])
                     }
-                    if(!ret)
-                        return ret;
-                    if(this.beforePublish !== undefined && jQuery.isFunction(this.beforePublish))
-                        if( !this.beforePublish(this.data) )
-                            return;
-                    var currentMethod = this.methods.save;
-                    if( parseInt(this.data.id) === -1 || this.data.id === undefined){
-                        currentMethod = this.methods.create;
-                        this.isnew = true;
+                }
+                if(e>0) { return; };
+                if(methods.before !== undefined && jQuery.isFunction(methods.before))
+                    if( !methods.before(this.fields) )
+                        return;
+                var currentMethod = actions.update;
+                if( parseInt(this.fields.id) === -1 || this.fields.id === undefined){
+                    currentMethod = actions.insert;
+                    this.isnew = true;
+                }
+                SimpleLog.log(this.data);
+                var self = this;
+                jQuery.post(
+                    url+'/'+currentMethod,
+                    this.fields,
+                    function() { },
+                    "json"
+                )
+                .done(function(data) {
+                    SimpleLog.log(data);
+                    if(self.isnew && data.id !== undefined) {
+                        self.isnew = false;
+                        self.fields.id = data.id;
                     }
-                    SimpleLog.log(this.data);
-                    var self = this;
-                    jQuery.post(
-                        this.url+'/'+currentMethod,
-                        this.data,
-                        function() {
-                        
-                        },
-                        "json"
-                    )
-                    .done(function(data) {
-                        SimpleLog.log(data);
-                        if(self.isnew && data.id !== undefined) {
-                            self.isnew = false;
-                            self.data.id = data.id;
-                        }
-                        if( self.done !== undefined && jQuery.isFunction(self.done))
-                            self.done(data);
-                    })
-                    .fail(function() {
-                        if( self.error !== undefined && jQuery.isFunction(self.error))
-                            self.error();
-                    });
-                },
-                'before': undefined,
-                'done': undefined,
-                'error': undefined
-            },
-            'init': function(data) {
-                var selfModel = this;
-                var d = new Date();
-                d = d.getTime()/100000;
-                d = ((d - parseInt(d))*100000)*parseInt(Math.random()*10);
-                selfModel.instance.id = "m"+parseInt(d);
-                SimpleLog.log(selfModel.instance.id);
-                data.each(function () {
-                    var input = jQuery(this);
-                    input.addClass(selfModel.instance.id+'-'+input.attr('name'));
-                    if( input.hasClass('ommit') ) return true;
-                    if( input.attr('type') === "checkbox" || input.attr('type') === "radio" ) {
-                        var group = jQuery("input[name='"+input.attr('name')+"']");
-                        var i = 0;
-                        input = jQuery('<input type="hidden" name="'+input.attr('name')+'" value="" />');
-                        input.insertBefore(group);
-                        group.each(function() {
-                            var special = jQuery(this);
-                            special.addClass('ommit');
-                            special.attr('name', special.attr('name')+i);
-                            special.change(function() {
-                                if( this.checked ) {
-                                    input.data( special.attr('name'), this.value );
-                                } else {
-                                    input.removeData( special.attr('name') );
-                                }
-                                input.val('');
-                                var j = 0;
-                                for( v in input.data() ) {
-                                    var s = ',';
-                                    if(j === 0)
-                                        s = '';
-                                    input.val(input.val()+s+input.data()[v]);
-                                }
-                                input.trigger('change');
-                            });
-                            if( special.attr('type') === 'checkbox' ) {
-                                i++;
-                            }
-                        });
-                        
-                    }
-                    selfModel.instance.data[input.attr("name")] = input.attr("value");
-                    input.change( function() {
-                        selfModel.instance.data[jQuery(this).attr('name')] = this.value;
-                        if( selfModel.instance.autopublish )
-                            selfModel.instance.publish();
-                    });
+                    if( methods.done !== undefined && jQuery.isFunction(methods.done))
+                        methods.done(data);
+                })
+                .fail(function(data) {
+                    if( methods.error !== undefined && jQuery.isFunction(methods.error))
+                        methods.error(data);
                 });
-                return selfModel.instance;
             }
         };
-        for( o in object ) {
-            if(o === 'fields' || o === 'id') continue;
-            model.instance[o] = object[o];
+        data.each(function () {
+            var input = jQuery(this);
+            input.addClass(d+'-'+input.attr('name'));
+            if( input.hasClass('ommit') ) return true;
+            if( input.attr('type') === "checkbox" || input.attr('type') === "radio" ) {
+                var group = jQuery("input[name='"+input.attr('name')+"']");
+                var i = 0;
+                input = jQuery('<input type="hidden" name="'+input.attr('name')+'" value="" />');
+                input.insertBefore(group);
+                group.each(function() {
+                    var special = jQuery(this);
+                    special.addClass('ommit').attr('name', special.attr('name')+i).change(function() {
+                        if( this.checked ) {
+                            input.data( special.attr('name'), this.value );
+                        } else {
+                            input.removeData( special.attr('name') );
+                        }
+                        input.val('');
+                        var j = 0;
+                        for( v in input.data() ) {
+                            var s = ',';
+                            if(j === 0)
+                                s = '';
+                            input.val(input.val()+s+input.data()[v]);
+                        }
+                        input.trigger('change');
+                    });
+                    if( special.attr('type') === 'checkbox' ) {
+                        i++;
+                    }
+                });
+            }
+            if( fields[input.attr("name")] !== undefined ) { /*Validar que el campo si pertenezca al modelo*/
+                oData.fields[input.attr("name")] = input.attr("value");
+                if( input.attr("value") === '' || input.attr("value") === undefined ) {
+                    if( fields[input.attr("name")].default !== undefined ) {
+                        oData.fields[input.attr("name")] = fields[input.attr("name")].default;
+                    }
+                }
+                input.attr('model-id', oData.id);
+                input.change( function() {
+                    if( autopublish ) {
+                        parent.validator(this);
+                    }
+                    oData.inputs.push(this);
+                    oData.fields[jQuery(this).attr('name')] = this.value;
+                    /*Publish*/
+                    if( autopublish ) {
+                        oData.publish(false);
+                    }
+                });
+            }
+        });
+        this.instances[oData.id] = oData;
+        return oData;
+    }; 
+    for(k in p) {
+        if( typeof(p[k]) === typeof(properties[k]) ) {
+            for( sk in p[k]) {
+                properties[k][sk] = p[k][sk];
+            }
+        } else {
+            properties[k] = p[k];
         }
-        return model;
-    }
+    };
 };
