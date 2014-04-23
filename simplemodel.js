51c79e50600ba8fcb83fe1948a2681bc86b50593
Sergio Alberto Vázquez <alb.vazq@gmail.com>
2014-04-23 01:13:10 -0500

    Version 1.9
    Many changes, fixed some bugs about the creation of fields for goups of
    elements like radiobuttons and checkboxes, addes handles to manage
    errors and warnings during publishing or even deleting, however this
    time the method to delete an instance has been removed, and will be
    added in the next version.

--- simplemodel.js
+++ simplemodel.js
@@ -7,49 +7,85 @@ var SimpleModel = function(p) {
             'delete': undefined
         },
         'methods': {
-            'before': undefined,
-            'done': undefined,
-            'error': undefined,
-            'beforeDelete': undefined,
-            'afterDelete': undefined
+        	'beforePublish': undefined,
+        	'afterPublish': undefined,
+        	'beforeDelete': undefined,
+        	'afterDelete': undefined
         },
-        'fields': undefined
+        'handlers': {
+        	'errorHandler': undefined,
+        	'warningHandler': undefined,
+        	'dataHandler': undefined,
+        	'domHandler': undefined,
+        	'validationHandler': undefined
+        }
+        'fields': undefined,
+        'autopublish': true,
+        'autovalidate': true,
+        'validator': 'defaul'
     };
-    for(k in p) {
+    this.instances = {
+    	'length': 0,
+        'onjects': undefined
+    };
+    this.validator = function(fields, domObjects) {
+    	var r = {
+    		'errors': 0,
+    		'warnings': 0
+    	}
+    	var validators = this.properties.validator.split("|");
+    	if(validators.indexOf("default") !== -1) {
+    		//Validation base
+    		for(i in domObjects) {
+    			var o = jQuery(domObjects[i])
+    			var v = domObjects[i].value;
+    			if( o.hasClass("border-error") ) { o.removeClass("border-error"); }
+        		if( o.hasClass("border-warning") ) { o.removeClass("border-warning"); }
+        		if( this.properties.fields[o.attr("name")] === undefined)
+            		continue;
+            	if( this.properties.fields[o.attr("name")].required !== undefined ) {
+		            if( v === '' || v === undefined) {
+		                o.addClass("border-error"); 
+		                r.errors++;
+		                continue
+		            }
+		        }
+		        if( this.properties.fields[o.attr("name")].type !== undefined ) {
+		            if( typeOf(v) !== this.properties.fields[o.attr("name")].type ) {
+		                o.addClass("border-warning");
+		                r.warnings++;
+		                continue;
+		            }
+		        }
+    		}
+    	}
+    	if( (validators.indexOf("handler") !== -1) && (jQuery.isFunction(this.properties.handlers.validationHandler)) ) {
+    		var hr = this.properties.handlers.validationHandler(fields, domObjects);
+    	}
+    	if(hr !== undefined) {
+    		if(hr.errors > 0) {
+    			r.errors += hr.errors;
+    		}
+    		if(hr.warnings > 0) {
+    			r.warnings += hr.warnings;
+    		}
+    	}
+    	return r;
+    };
+    for(var k in p) {
         if( typeof(p[k]) === typeof(this.properties[k]) ) {
-            for( sk in p[k]) {
+            for(var sk in p[k]) {
                 this.properties[k][sk] = p[k][sk];
             }
         } else {
             this.properties[k] = p[k];
         }
-    };
-    this.instances = {};
-    this.insN = 0;
-    this.validator = function(i) {
-        if( jQuery(i).hasClass("border-error") ) { jQuery(i).removeClass("border-error"); }
-        if( jQuery(i).hasClass("border-warning") ) { jQuery(i).removeClass("border-warning"); }
-        if( this.properties.fields[jQuery(i).attr("name")] === undefined)
-            return 0;
-        if( this.properties.fields[jQuery(i).attr("name")].required !== undefined ) {
-            if( i.value === '' || i.value === undefined) {
-                jQuery(i).addClass("border-error");
-                SimpleLog.error("Validator class SimpleModel: "+jQuery(i).attr("name"));
-                return 1;
-            }
-        }
-        if( this.properties.fields[jQuery(i).attr("name")].type !== undefined ) {
-            if( typeOf(i.value) !== this.properties.fields[jQuery(i).attr("name")].type ) {
-                jQuery(i).addClass("border-warning");
-                SimpleLog.warn("Validator class SimpleModel: "+jQuery(i).attr("name"));
-                return 1;
-            }
-        }
-        return 0;
-    };
-    this.create = function(data, autopublish) {
+    }
+    
+    this.create = function(data) {
         var parent = this;
         var methods = parent.properties.methods;
+        var handlers = parent.properties.handlers;
         var actions = parent.properties.actions;
         var d = new Date();
         var id = 0;
@@ -59,137 +95,125 @@ var SimpleModel = function(p) {
         /*Instance, this will be returned to manage the new object that belongs to the model*/
         var Instance = {
             'id': id,
-            'autopublish': autopublish,
-            'fields': {},
-            'isnew': false,
-            'before': undefined,
-            'inputs': [],
-            'publish': function(validate, autopublish, callback) {
-                var e = 0;
-                this.autopublish = autopublish;
-                if(validate) {
-                    SimpleLog.warn("Entrando", "Publishing instance");
-                    for(i in this.inputs) {
-                        e = e+parent.validator(this.inputs[i]);
+            'data': {},
+            'fields': [],
+            'publish': function() {
+            	var isnew = false;
+            	if( jQuery.isFunction(methods.beforePublish) ) {
+            		var bp = methods.before(this.fields);
+                    if( !bp ) {
+                    	if( jQuery.isFunction(handlers.errorHandler) ) {
+							handlers.errorHandler("ERROR_BEFORE_PUBLISH");
+						}
                     }
                 }
-                if(e>0) { SimpleLog.error("No guarda", "Publishing instance"); return e; };
-                if(methods.before !== undefined && jQuery.isFunction(methods.before)) {
-                    if( !methods.before(this.fields) ) {
-                        return 'before_fail';
-                    }
+                if(parent.properties.autovalidate) {
+                	var r = parent.validator();
+					if(r.errors > 0) {
+						if( jQuery.isFunction(handlers.errorHandler) ) {
+							handlers.errorHandler("ERROR_ON_VALIDATION");
+						}
+					} 
+					if(r.warnings > 0) {
+						if( jQuery.isFunction(handlers.warningHandler) ) {
+							handlers.warningHandler("WARNING_ON_VALIDATION");
+						}
+					}
                 }
-                var currentMethod = actions.update;
-                
-                if( parseInt(this.fields.id) === -1 || this.fields.id === undefined){
+                var mainMethod = actions.update;
+                if( this.data.id === undefined ) {
                     currentMethod = actions.insert;
-                    this.isnew = true;
+                    isnew = true;
                 }
-                SimpleLog.warn(currentMethod, "Current method");
-                SimpleLog.log(this.fields, "This.Data");
                 var self = this;
                 jQuery.post(
-                    parent.properties.url+'/'+currentMethod,
-                    self.fields,
-                    function() { },
+                    parent.properties.url+'/'+mainMethod,
+                    self.data,
                     "json"
                 )
                 .done(function(data) {
-                    SimpleLog.log(data, "Publish instance class SimpleModel");
-                    if(self.isnew && data.id !== undefined) {
-                        self.isnew = false;
-                        self.fields.id = data.id;
+                    if( isnew && data.id !== undefined) {
+                        isnew = false;
+                        self.data.id = data.id;
                     }
-                    if( methods.done !== undefined && jQuery.isFunction(methods.done)){
-                        methods.done(data, self.fields);
-                        if(jQuery.isFunction(callback)) {
-                            callback();
+                    if( jQuery.isFunction(methods.afterPublish)){
+                    	var ap = methods.afterPublish(data, self.data, self.fields);
+                    	if( !ap ) {
+                        	if( jQuery.isFunction(handlers.errorHandler ) {
+								handlers.warningHandler("ERROR_AFTER_PUBLISH");
+							}
                         }
                     }
                 })
                 .fail(function(data) {
-                    if( methods.error !== undefined && jQuery.isFunction(methods.error)) {
-                        methods.error(data);
-                    }
+                	if( jQuery.isFunction(handlers.errorHandler ) {
+						handlers.errorHandler("PUBLISH_FAIL", data);
+					}
                 });
-                return true;
             }, 
             'delete': function() {
-                jQuery.post(
-                    parent.properties.url+'/'+actions.delete,
-                    this.fields.id,
-                    function() { },
-                    "json"
-                )
-                .done(function(data) {
-                    SimpleLog.log("Delete instance class SimpleModel: "+data);
-                    if(self.isnew && data.id !== undefined) {
-                        self.isnew = false;
-                        self.fields.id = data.id;
-                    }
-                    if( methods.done !== undefined && jQuery.isFunction(methods.done))
-                        methods.done(data, self.fields);
-                })
-                .fail(function(data) {
-                    if( methods.error !== undefined && jQuery.isFunction(methods.error))
-                        methods.error(data);
-                });
+                
             }
         };
-        /*This part process all the data attached to the new instance and assign the events nedded*/
         data.each(function () {
             var input = jQuery(this);
-            input.addClass(id+'-'+input.attr('name'));
-            if( input.hasClass('ommit') ) return true;
-            if( input.attr('type') === "checkbox" || input.attr('type') === "radio" ) {
-                var group = jQuery("input[name='"+input.attr('name')+"']");
-                var i = 0;
-                input = jQuery('<input type="hidden" name="'+input.attr('name')+'" value="" />');
-                input.insertBefore(group);
-                group.each(function() {
-                    var special = jQuery(this);
-                    special.addClass('ommit').attr('name', special.attr('name')+i).change(function() {
-                        if( this.checked ) {
-                            input.data( special.attr('name'), this.value );
-                        } else {
-                            input.removeData( special.attr('name') );
-                        }
-                        input.val('');
-                        var j = 0;
-                        for( v in input.data() ) {
+            input.attr("model-id", id);
+            if( input.hasClass('ommit') ) return;
+            if( parent.properties.fields[input.attr("name")] !== undefined ) {
+            	
+            	if( input.attr('type') === "checkbox" || input.attr('type') === "radio" ) {
+	                var group = jQuery("input[name='"+input.attr('name')+"']");
+	                var i = 0;
+	                input = jQuery('<input type="hidden" name="'+input.attr('name')+'" value="" />');
+	                input.insertBefore(group);
+	                var dataProccessor = function(i) {
+	                	i.val('');
+	                	var j = 0;
+                        for( var v in i.data() ) {
                             var s = ',';
                             if(j === 0)
                                 s = '';
-                            input.val(input.val()+s+input.data()[v]);
+                            i.val(i.val()+s+i.data()[v]);
                         }
-                        input.trigger('change');
-                    });
-                    if( special.attr('type') === 'checkbox' ) {
-                        i++;
-                    }
-                });
-            }
-            if( parent.properties.fields[input.attr("name")] !== undefined ) { /*Validar que el campo si pertenezca al modelo*/
-                Instance.fields[input.attr("name")] = input.attr("value");
-                if( input.attr("value") === '' || input.attr("value") === undefined ) {
-                    if( parent.properties.fields[input.attr("name")].default !== undefined ) {
-                        Instance.fields[input.attr("name")] = parent.properties.fields[input.attr("name")].default;
-                    }
-                }
-                input.attr('model-id', Instance.id);
-                input.change( function() {
-                    Instance.fields[jQuery(this).attr('name')] = this.value;
-                    /*Publish*/
-                    if( Instance.autopublish ) {
-                        Instance.publish(true, true);/*Validar y seguir autopublish*/
-                    }
-                });
-                Instance.inputs.push(this);
+                        i.trigger('change');
+	                }
+	                group.each(function() {
+	                    var special = jQuery(this);
+	                    if(this.checked) {
+	                    	input.data( special.attr('name'), this.value );
+	                    }
+	                    special.addClass('ommit').attr('name', special.attr('name')+i).change(function() {
+	                        if( this.checked ) {
+	                            input.data( special.attr('name'), this.value );
+	                        } else {
+	                            input.removeData( special.attr('name') );
+	                        }
+	                        dataProccessor(input);
+	                    });
+	                    if( special.attr('type') === 'checkbox' ) {
+	                        i++;
+	                    }
+	                });
+	            } else {
+	            	Instance.data[input.attr("name")] = input.attr("value");
+	                if( input.attr("value") === '' || input.attr("value") === undefined ) {
+	                    if( parent.properties.fields[input.attr("name")].default !== undefined ) {
+	                        Instance.data[input.attr("name")] = parent.properties.fields[input.attr("name")].default;
+	                    }
+	                }
+	            }   
+	            input.change( function() {
+	                Instance.fields[jQuery(this).attr('name')] = this.value;
+	                if( parent.autopublish ) {
+	                    Instance.publish();
+	                }
+	            });
+	            dataProccessor(input);
+                Instance.inputs.push(input);
             }
         });
-        SimpleLog.log(this.instances, "SimpleModel Instances");
-        this.instances[Instance.id] = Instance;
-        this.insN = this.insN + 1;
+		this.instances.length++;
+        this.instances.objects[Instance.id] = Instance;
         return Instance;
     }; 
     
